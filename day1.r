661ef209231376c3aa8ce4b2312815c2f85cbe85
DineshSanklecha <73770671+DineshSanklecha@users.noreply.github.com>
2020-11-01 11:59:51 +0530

    Add files via upload

---
+++ day1.r
@@ -0,0 +1,104 @@
+
+R version 4.0.3 (2020-10-10) -- "Bunny-Wunnies Freak Out"
+Copyright (C) 2020 The R Foundation for Statistical Computing
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+> 1+1
+> 2^2
+> pi
+> 0*pi
+> 2-pi
+> 2*pi
+> hello team
+> "hello team"
+> "This is R for Data Science"
+> c=a+b
+> a<-10
+> b<-5
+> c=a+b
+> c= a+b
+> c<-a+b
+> a
+> b
+> a+b
+> c<-(a+b)
+> c
+> exp(2)
+> 2%%2
+> c(1,2,3,4,5,6,7)
+> age<-c(25,25,23,26,30,60)
+> Name<-c("ram","sham","tom","raja","koller","sam")
+> result<-age+name
+> x<-c(1,2,3)
+> y<-c(4,5,6)
+> x+y
+> x*y
+> sqrt(x)
+> c<-x*y
+> sqrt(c)
+> x[1]
+> x[2]
+> y[1]
+> x[1]<-0
+> x[1]
+> x+y
+
+y<-c(2,3,4)
+y[c(1,2)]
+y[y<9]
+y<9
+y[y<9]=9
+y
+y<-c(2,3,4)
+y[-1]<-4
+y
+x<-c(1,2,3)
+x[-1]<-0
+x
+z=1
+z
+z=c(1,2,3)
+z
+d<-c(1,2,3,4,56,67)
+d
+d[-5]<-0
+d
+my_df<-data.frame(x<-c(1,2,3),y<-c('a','b','c'))
+my_df           
+
+my_df<-data.frame(x=c(1,2,3),y=c('a','b','c'))
+mf_df
+my_df
+tom<-data.frame(age=13, address='89,wall street')
+tom
+tom[1,2]
+df<-data.frame(x=1:3, y=c('a','b','c'))
+df
+df[1,1]
+df[1,]
+df[1,2]
+df[3,2]
+df[1,1]
+df[3,1]
+df[1,1]
+df[1,2]
+df[3,1]
+df[3,2]
+df[c(1,3),2]
+df[c(1,2),1]
+df[c(1,3),]
+
